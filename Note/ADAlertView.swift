fe04d7d7b0572f7597b615f3c8963d247977eac0
huangboju <529940945@qq.com>
2016-06-25 18:53:47 +0800

    整理代码

--- Note/ADAlertView.swift
+++ Note/ADAlertView.swift
@@ -11,26 +11,20 @@ class ADAlertCloseButton: UIButton {
     }
 
     override func drawRect(rect: CGRect) {
-        let buttonWidth = min(CGRectGetWidth(rect), CGRectGetHeight(rect))
+        let buttonWidth = min(rect.width, rect.height)
         let radius = buttonWidth / 2
         layer.cornerRadius = radius
         layer.masksToBounds = true
         let contex = UIGraphicsGetCurrentContext()
         CGContextSetStrokeColorWithColor(contex, UIColor.lightGrayColor().CGColor)
         CGContextSetLineWidth(contex, 1)
-
         CGContextBeginPath(contex)
-
-        let path = UIBezierPath(roundedRect: CGRectInset(rect, 1, 1), cornerRadius: radius).CGPath
-
+        let path = UIBezierPath(roundedRect: rect.insetBy(dx: 1, dy: 1), cornerRadius: radius).CGPath
         CGContextAddPath(contex, path)
-
         CGContextMoveToPoint(contex, 0, 0)
-        CGContextAddLineToPoint(contex, CGRectGetMaxX(rect), CGRectGetMaxY(rect))
-
-        CGContextMoveToPoint(contex, CGRectGetMaxX(rect), 0)
-        CGContextAddLineToPoint(contex, 0, CGRectGetMaxY(rect))
-
+        CGContextAddLineToPoint(contex, rect.maxX, rect.maxY)
+        CGContextMoveToPoint(contex, rect.maxX, 0)
+        CGContextAddLineToPoint(contex, 0, rect.maxY)
         CGContextStrokePath(contex)
     }
 
@@ -46,7 +40,7 @@ class ADAlertContainerView: UIView, UICollectionViewDelegate, UICollectionViewDa
     var closeBtnTintColor: UIColor?
     var closeBtnBgColor: UIColor?
     var contents = [UIImage]()
-    
+
     private let kCloseButtonWidth: CGFloat = 24
     private let lengthen: CGFloat = 15
     private let closeBtn = ADAlertCloseButton()
@@ -54,11 +48,11 @@ class ADAlertContainerView: UIView, UICollectionViewDelegate, UICollectionViewDa
     private let layout = UICollectionViewFlowLayout()
     private var collectionView: UICollectionView!
     private var selectedIndexPath: ((NSIndexPath) -> Void)?
-    
+
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
-        
+
+
         collectionView = UICollectionView(frame: frame, collectionViewLayout: UICollectionViewLayout())
         collectionView.delegate = self
         collectionView.dataSource = self
@@ -69,65 +63,65 @@ class ADAlertContainerView: UIView, UICollectionViewDelegate, UICollectionViewDa
         layout.scrollDirection = .Horizontal
         layout.minimumLineSpacing = 0
         collectionView.collectionViewLayout = layout
-        
+
         closeBtn.buttonStrokeColor = closeBtnTintColor
         closeBtn.backgroundColor = .clearColor()
         closeBtn.addTarget(self.superview, action: #selector(ADAlertView().hide), forControlEvents: .TouchUpInside)
-        
+
         if let cornerRadius = self.cornerRadius {
             containerView.layer.cornerRadius = cornerRadius
         }
-        
+
         containerView.backgroundColor = containerBgColor
         containerView.layer.masksToBounds = true
-        
+
         containerView.addSubview(collectionView)
-        
+
         addSubview(containerView)
         addSubview(closeBtn)
     }
-    
+
     override func layoutSubviews() {
         super.layoutSubviews()
         containerView.frame = bounds
-        
+
         closeBtn.bounds = CGRect(origin: CGPoint.zero, size: CGSize(width: kCloseButtonWidth, height: kCloseButtonWidth))
         closeBtn.frame.origin = CGPoint(x: (containerView.frame.maxX - kCloseButtonWidth) / 2, y: containerView.frame.maxY - kCloseButtonWidth)
         closeBtn.setNeedsDisplay()
-        
+
         collectionView.frame = containerView.bounds.insetBy(dx: 0, dy: kCloseButtonWidth + lengthen)
         layout.itemSize = collectionView.frame.size
-        
+
         collectionView.reloadData()
         collectionView.contentOffset = CGPoint.zero
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     // MARK: - UICollectionViewDataSource
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return contents.count
     }
-    
+
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("cell_id", forIndexPath: indexPath)
         return cell
     }
-    
+
     // MARK: - UICollectionViewDelegate
     func collectionView(collectionView: UICollectionView, willDisplayCell cell: UICollectionViewCell, forItemAtIndexPath indexPath: NSIndexPath) {
         let adCell = cell as? ADCell
         adCell?.imageView.image = contents[indexPath.row]
     }
-    
+
     func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
         if let selectedIndexPath = self.selectedIndexPath {
             selectedIndexPath(indexPath)
         }
     }
-    
+
     func selected(item: ((NSIndexPath) -> Void)) {
         self.selectedIndexPath = item
     }
@@ -136,8 +130,8 @@ class ADAlertContainerView: UIView, UICollectionViewDelegate, UICollectionViewDa
 class ADCell: UICollectionViewCell {
     let imageView = UIImageView()
     let textLabel = UILabel()
-    
-    
+
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         imageView.frame = CGRect(origin: CGPoint.zero, size: frame.size)
@@ -145,11 +139,11 @@ class ADCell: UICollectionViewCell {
         imageView.contentMode = .ScaleAspectFill
         clipsToBounds = true
         contentView.addSubview(imageView)
-        
+
         textLabel.frame = CGRect(x: (frame.width - 40) / 2, y: 50, width: 40, height: 20)
         contentView.addSubview(textLabel)
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -164,7 +158,7 @@ class ADAlertView: UIView {
     var minHorizontalPadding: CGFloat?
     var minVertalPadding: CGFloat?
     var proportion: CGFloat?
-    
+
     var containerSubviews = [UIImage]() {
         didSet {
             if NSThread.isMainThread() {
@@ -174,18 +168,18 @@ class ADAlertView: UIView {
             }
         }
     }
-    
+
     private var selectedIndexPath: ((NSIndexPath) -> Void)?
     private var closeAction: ((Bool) -> Void)?
-    
+
     private let containerView = ADAlertContainerView()
-    
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         alpha = 0
-        
+
         backgroundColor = .clearColor()
-        
+
         //        cardBgColor = .whiteColor()
         closeBtnTintColor = .whiteColor()
         //        closeBtnBgColor = .whiteColor()
@@ -195,40 +189,40 @@ class ADAlertView: UIView {
         minVertalPadding = 10
         proportion = 0.75
         addSubview(containerView)
-        
+
         NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(statusBarOrientationDidChange), name: UIApplicationDidChangeStatusBarOrientationNotification, object: nil)
     }
-    
+
     convenience init () {
         self.init(frame: CGRect.zero)
     }
-    
+
     convenience init(view: UIView, handle: ((NSIndexPath) -> Void)? = nil, close: ((Bool) -> Void)? = nil) {
         self.init(frame: view.bounds)
         view.addSubview(self)
         selectedIndexPath = handle
         closeAction = close
     }
-    
+
     convenience init(window: UIWindow) {
         self.init(view: window)
     }
-    
+
     deinit {
         NSNotificationCenter.defaultCenter().removeObserver(self, name: UIApplicationDidChangeStatusBarOrientationNotification, object: nil)
     }
-    
+
     func show() {
         showUsingAnimation(true)
     }
-    
+
     func hide() {
         if let close = self.closeAction {
             close(true)
         }
         hideUsingAnimation(true)
     }
-    
+
     func showUsingAnimation(animated: Bool) {
         if animated {
             UIView.animateWithDuration(0.2, animations: {
@@ -238,7 +232,7 @@ class ADAlertView: UIView {
             alpha = 1
         }
     }
-    
+
     func hideUsingAnimation(animated: Bool) {
         if animated {
             UIView.animateWithDuration(0.2, animations: {
@@ -248,7 +242,7 @@ class ADAlertView: UIView {
             alpha = 0
         }
     }
-    
+
     func updateUIForKeypath() {
         containerView.contents = containerSubviews
         containerView.selected({ [unowned self] (indexPath) in
@@ -257,23 +251,23 @@ class ADAlertView: UIView {
                 self.hide()
             }
             })
-        
+
         setNeedsLayout()
         setNeedsDisplay()
-        
+
         for subView in subviews {
             subView.setNeedsLayout()
             subView.setNeedsDisplay()
         }
     }
-    
+
     func statusBarOrientationDidChange(notification: NSNotification) {
         if let superview = self.superview {
             frame = superview.bounds
             setNeedsDisplay()
         }
     }
-    
+
     override func drawRect(rect: CGRect) {
         if dimBackground! {
             let context = UIGraphicsGetCurrentContext()
@@ -281,18 +275,20 @@ class ADAlertView: UIView {
             CGContextFillRect(context, rect)
         }
     }
-    
+
+
+    //layoutSubviews是调整自己子视图们的frame；而drawRect是创建自己的视图内容
     override func layoutSubviews() {
         let kWidthPadding = minHorizontalPadding
         let kHeightPadding = minVertalPadding
-        
+
         let kProportion = proportion
-        
+
         containerView.containerBgColor = cardBgColor
         containerView.closeBtnBgColor = closeBtnBgColor
         containerView.closeBtnTintColor = closeBtnTintColor
         containerView.cornerRadius = cornerRadius
-        
+
         if bounds.width > bounds.height {
             let containerHeight = bounds.height - kHeightPadding! * 2
             containerView.bounds = CGRect(origin: CGPoint.zero, size: CGSize(width: containerHeight * kProportion!, height: containerHeight))
@@ -300,10 +296,10 @@ class ADAlertView: UIView {
             let containerWidth = bounds.width - kWidthPadding! * 2
             containerView.bounds = CGRect(origin: CGPoint.zero, size: CGSize(width: containerWidth, height: containerWidth / kProportion! + 78))
         }
-        
+
         containerView.center = CGPoint(x: bounds.midX, y: bounds.midY - 39)
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
