b997ca8ba1d293c838e8a4825e4b2967ab4e99c2
huangboju <huangboju@huazhu.com>
2017-11-03 16:58:22 +0800

    添加alertcontroller

---
+++ Examples/AlertController/AlertController/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  AlertController
+//
+//  Created by 伯驹 黄 on 2016/10/10.
+//  Copyright © 2016年 伯驹 黄. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+
+        let button = UIButton(frame: view.frame.insetBy(dx: 100, dy: 200))
+        button.addTarget(self, action: #selector(action), for: .touchUpInside)
+        button.backgroundColor = .green
+        view.addSubview(button)
+    }
+
+    func action() {
+        let alertController = UIAlertController(title: "", message: "", preferredStyle: .alert)
+        let ok = UIAlertAction(title: "确定", style: .default) { _ in
+        }
+        alertController.addAction(ok)
+
+        let image = UIImage(named: "ic_attachment")
+        let attachment = NSTextAttachment()
+        attachment.image = image
+        attachment.bounds = CGRect(x: 0, y: 0, width: 30, height: 30)
+
+        let alertControllerTitleStr = NSMutableAttributedString(string: "测试")
+        alertControllerTitleStr.append(NSAttributedString(attachment: attachment))
+        alertControllerTitleStr.addAttribute(NSForegroundColorAttributeName, value: UIColor.green, range: NSRange(location: 0, length: 2))
+        alertController.setValue(alertControllerTitleStr, forKey: "attributedTitle")
+        let alertControllerMessageStr = NSMutableAttributedString(string: "今天天气好晴朗")
+        alertControllerMessageStr.addAttribute(NSForegroundColorAttributeName, value: UIColor.red, range: NSRange(location: 0, length: 4))
+        alertController.setValue(alertControllerMessageStr, forKey: "attributedMessage")
+        ok.setValue(UIColor.red, forKey: "titleTextColor")
+        present(alertController, animated: true) {
+        }
+    }
+}
