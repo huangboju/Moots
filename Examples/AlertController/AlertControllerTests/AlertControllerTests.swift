b5803d04bfd45478fbe676b31b4a9cbdd698ddd5
huangboju <huangboju@huazhu.com>
2017-11-03 16:58:22 +0800

    添加alertcontroller

---
+++ Examples/AlertController/AlertControllerTests/AlertControllerTests.swift
@@ -0,0 +1,35 @@
+//
+//  AlertControllerTests.swift
+//  AlertControllerTests
+//
+//  Created by 伯驹 黄 on 2016/10/10.
+//  Copyright © 2016年 伯驹 黄. All rights reserved.
+//
+
+import XCTest
+@testable import AlertController
+
+class AlertControllerTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+}
