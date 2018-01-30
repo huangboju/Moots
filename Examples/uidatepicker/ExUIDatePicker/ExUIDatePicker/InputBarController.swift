2b53d942b369194fee3c9b75708d15ad8cbf28ec
huangboju <bhuang@xiaohongshu.com>
2018-01-30 15:28:28 +0800

    添加undomanager

--- Examples/uidatepicker/ExUIDatePicker/ExUIDatePicker/InputBarController.swift
+++ Examples/uidatepicker/ExUIDatePicker/ExUIDatePicker/InputBarController.swift
@@ -10,20 +10,83 @@ import UIKit
 
 let width = UIScreen.main.bounds.width
 
-class InputBar: UIView {
-    let textView = UITextField(frame: CGRect(x: 50, y: 7, width: width - 100, height: 30))
+// https://stackoverflow.com/questions/25816994/changing-the-frame-of-an-inputaccessoryview-in-ios-8
+
+class InputBar: UIView, UITextViewDelegate {
+    
+    lazy var textView: UITextView = {
+        let textView = UITextView()
+        textView.delegate = self
+        textView.layer.borderWidth = 1
+        textView.layer.cornerRadius = 5
+        textView.layer.borderColor = UIColor.lightGray.cgColor
+        textView.isScrollEnabled = false
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
+    }()
+    
+    private lazy var visualView: UIVisualEffectView = {
+        let blurEffect = UIBlurEffect(style: .extraLight)
+        let visualView = UIVisualEffectView(effect: blurEffect)
+        visualView.translatesAutoresizingMaskIntoConstraints = false
+        return visualView
+    }()
+    
+    private lazy var emojiButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(named: "icon_emotion"), for: .normal)
+        button.addTarget(self, action: #selector(showEmojiKeyboard), for: .touchUpInside)
+        return button
+    }()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
 
-        let blurEffect = UIBlurEffect(style: .extraLight)
-        let visualView = UIVisualEffectView(effect: blurEffect)
-        visualView.frame = frame
-        addSubview(visualView)
+        autoresizingMask = .flexibleHeight
 
         backgroundColor = .red
-        textView.borderStyle = .roundedRect
+        
+        addSubview(visualView)
+        visualView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        visualView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        visualView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        visualView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        
+
         addSubview(textView)
+        textView.topAnchor.constraint(equalTo: topAnchor, constant: 8).isActive = true
+        textView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        textView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 50).isActive = true
+        textView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        
+        addSubview(emojiButton)
+        emojiButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8).isActive = true
+        emojiButton.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        invalidateIntrinsicContentSize()
+    }
+    
+    @objc
+    func showEmojiKeyboard(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        if sender.isSelected {
+            let keyboardView = UIView(frame: CGRect(x: 0, y: 0, width: width, height: 200))
+            keyboardView.backgroundColor = .red
+            textView.resignFirstResponder()
+            textView.inputView = keyboardView
+        } else {
+            textView.inputView = nil
+        }
+        textView.reloadInputViews()
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        // Calculate intrinsicContentSize that will fit all the text
+        let textSize = textView.contentSize
+        return CGSize(width: bounds.width, height: textSize.height)
     }
 
     required init?(coder aDecoder: NSCoder) {
---
+++ Examples/undo-manager-practice-master/UndoAppExample/BoardView.swift
@@ -0,0 +1,13 @@
+//
+//  BoardView.swift
+//  UndoAppExample
+//
+//  Created by Tomasz Szulc on 12/09/15.
+//  Copyright © 2015 Tomasz Szulc. All rights reserved.
+//
+
+import UIKit
+
+class BoardView: UIView {
+
+}
