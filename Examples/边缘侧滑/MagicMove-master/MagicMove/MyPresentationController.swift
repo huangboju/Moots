6a78e19d338051745fdaec92d9141fd2800ff5b0
huangboju <bhuang@xiaohongshu.com>
2018-06-13 17:05:50 +0800

    加入UIPresentationController

--- Examples/边缘侧滑/MagicMove-master/MagicMove/MyPresentationController.swift
+++ Examples/边缘侧滑/MagicMove-master/MagicMove/MyPresentationController.swift
@@ -6,6 +6,60 @@
 //  Copyright © 2018 Bourne. All rights reserved.
 //
 
-//class MyPresentationController: UIPresentationController {
-//    
-//}
+import UIKit
+
+class MyPresentationController: UIPresentationController {
+    
+    override var presentationStyle: UIModalPresentationStyle {
+        return .custom
+    }
+    
+    private lazy var dimmingView: UIView = {
+        let dimmingView = UIView()
+        dimmingView.translatesAutoresizingMaskIntoConstraints = false
+        dimmingView.backgroundColor = UIColor(white: 0, alpha: 0.5)
+        dimmingView.alpha = 0
+        return dimmingView
+    }()
+    
+    override var frameOfPresentedViewInContainerView: CGRect {
+        var frame: CGRect = .zero
+        frame.size = size(forChildContentContainer: presentedViewController, withParentContainerSize: containerView!.bounds.size)
+        frame.origin.x = 100
+        return frame
+    }
+
+    override func presentationTransitionWillBegin() {
+        containerView?.insertSubview(dimmingView, at: 0)
+        NSLayoutConstraint.activate(NSLayoutConstraint.constraints(withVisualFormat: "V:|[dimmingView]|", options: [], metrics: nil, views: ["dimmingView": dimmingView]))
+        NSLayoutConstraint.activate(NSLayoutConstraint.constraints(withVisualFormat: "H:|[dimmingView]|", options: [], metrics: nil, views: ["dimmingView": dimmingView]))
+        
+        guard let coordinator = presentedViewController.transitionCoordinator else {
+            dimmingView.alpha = 1.0
+            return
+        }
+
+        coordinator.animate(alongsideTransition: { _ in
+            self.dimmingView.alpha = 1.0
+        })
+    }
+    
+    override func dismissalTransitionWillBegin() {
+        guard let coordinator = presentedViewController.transitionCoordinator else {
+            dimmingView.alpha = 0.0
+            return
+        }
+        
+        coordinator.animate(alongsideTransition: { _ in
+            self.dimmingView.alpha = 0.0
+        })
+    }
+
+    override func containerViewWillLayoutSubviews() {
+        presentedView?.frame = frameOfPresentedViewInContainerView
+    }
+
+    override func size(forChildContentContainer container: UIContentContainer, withParentContainerSize parentSize: CGSize) -> CGSize {
+        return CGSize(width: parentSize.width - 100, height: parentSize.height)
+    }
+}
--- Examples/边缘侧滑/MagicMove-master/MagicMove/Transition/PresentingTransion.swift
+++ Examples/边缘侧滑/MagicMove-master/MagicMove/Transition/PresentingTransion.swift
@@ -10,30 +10,28 @@ import UIKit
 
 class PresentingTransion: NSObject, UIViewControllerAnimatedTransitioning {
     func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
-        UIPresentationController
-        return 0.5
+        return 0.25
     }
     
     func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
         //1.获取动画的源控制器和目标控制器
-        guard let toView = transitionContext.view(forKey: .to) else {
+        guard let toView = transitionContext.view(forKey: .to),
+              let toVC = transitionContext.viewController(forKey: .to) else {
             return
         }
+        
+        let presentedFrame = transitionContext.finalFrame(for: toVC)
 
         let containerView = transitionContext.containerView
-        containerView.backgroundColor = UIColor(white: 0, alpha: 0)
         containerView.addSubview(toView)
 
         let containerViewSize = containerView.frame.size
 
         let x = containerViewSize.width - toView.bounds.minX
         toView.frame.origin.x = x
-        let endX: CGFloat = 100
-        toView.frame.size = CGSize(width: containerViewSize.width - endX, height: containerViewSize.height)
 
         UIView.animate(withDuration: transitionDuration(using: transitionContext), delay: 0, options: .curveEaseInOut, animations: {
-            toView.frame.origin.x = endX
-            containerView.backgroundColor = UIColor(white: 0, alpha: 0.5)
+            toView.frame = presentedFrame
         }, completion: { _ in
             transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
         })
