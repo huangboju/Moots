2a5c7b04704cc5cda11c3ffe1a25622b28d1751f
huangboju <bhuang@xiaohongshu.com>
2018-06-13 14:01:00 +0800

    加入侧滑出现vc

--- Examples/边缘侧滑/MagicMove-master/MagicMove/Transition/MagicMovePushTransion.swift
+++ Examples/边缘侧滑/MagicMove-master/MagicMove/Transition/MagicMovePushTransion.swift
@@ -10,34 +10,30 @@ import UIKit
 
 class MagicMovePushTransion: NSObject, UIViewControllerAnimatedTransitioning {
     func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
-        return 0.5
+        return TimeInterval(UINavigationControllerHideShowBarDuration)
     }
     
     func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
         //1.获取动画的源控制器和目标控制器
-        guard let toView = transitionContext.view(forKey: .to),
-            let fromView = transitionContext.view(forKey: .from) else {
+        guard let toView = transitionContext.view(forKey: .to) else {
             return
         }
-        let screenBounds = UIScreen.main.bounds
-        
-        let x      = screenBounds.width - toView.bounds.minX
-        fromView.alpha = 0.2
-
-        toView.frame.origin.x = x
 
         let containerView = transitionContext.containerView
-        
-        containerView.insertSubview(toView, belowSubview: fromView)
-        
-        UIView.animate(withDuration: transitionDuration(using: transitionContext), delay: 0, options: .curveEaseInOut, animations: {
-            fromView.frame.origin.x = x
-            fromView.alpha = 1
+        containerView.backgroundColor = UIColor(white: 0, alpha: 0)
+        containerView.addSubview(toView)
 
-            toView.frame.origin.x = 0
+        let containerViewSize = containerView.frame.size
+
+        let x = containerViewSize.width - toView.bounds.minX
+        toView.frame.origin.x = x
+        let endX: CGFloat = 100
+        toView.frame.size = CGSize(width: containerViewSize.width - endX, height: containerViewSize.height)
+
+        UIView.animate(withDuration: transitionDuration(using: transitionContext), delay: 0, options: .curveEaseInOut, animations: {
+            toView.frame.origin.x = endX
+            containerView.backgroundColor = UIColor(white: 0, alpha: 0.5)
         }, completion: { _ in
-            fromView.frame.origin.x = 0
-            fromView.removeFromSuperview()
             transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
         })
     }
