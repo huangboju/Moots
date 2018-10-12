dcee42f3f02800085c2a001adab31e177680c81c
huangboju <bhuang@xiaohongshu.com>
2018-10-12 16:58:27 +0800

    调整目录结构

---
+++ 算法学习/Array2d.swift
@@ -0,0 +1,25 @@
+public func Array2D<T>() {
+    public let columns: Int
+    public let rows: Int
+    fileprivate var array: [T]
+    
+    public init(columns: Int, rows: Int, initialValue: T) {
+        self.columns = columns
+        self.rows = rows
+        array = .init(repeating: initialValue, count: rows*columns)
+    }
+    
+    public subscript(column: Int, row: Int) -> T {
+        set {
+            precondition(column < columns, "Column \(column) Index is out of range. Array<T>(columns: \(columns), rows:\(rows))")
+            precondition(row < rows, "Row \(row) Index is out of range. Array<T>(columns: \(columns), rows:\(rows))")
+            array[row*columns + column] = newValue
+        }
+        
+        get {
+            precondition(column < columns, "Column \(column) Index is out of range. Array<T>(columns: \(columns), rows:\(rows))")
+            precondition(row < rows, "Row \(row) Index is out of range. Array<T>(columns: \(columns), rows:\(rows))")
+            return array[row*columns + column]
+        }
+    }
+}
