5e9b1e0affc3b9b1428b9801af8c85fc70318d18
huangboju <bhuang@xiaohongshu.com>
2018-10-29 20:02:30 +0800

    删除没用代码

--- 算法学习/LeetCode/LeetCode/MinCount.swift
+++ 算法学习/LeetCode/LeetCode/MinCount.swift
@@ -12,21 +12,20 @@ func makeChange(_ values: [Int], _ money: Int) {
     // 保存每一个面值找零所需的最小硬币数，0号单元舍弃不用，所以要多加1
     var coinsUsed = [Int](repeating: 0, count: money + 1)
     var coinTrack = [Int](repeating: 0, count: money + 1)
-    coinsUsed[0] = 0
     var last = 0
     // 对每一分钱都找零，即保存子问题的解以备用，即填表
     for cents in 1 ... money {
         // 当用最小币值的硬币找零时，所需硬币数量最多
         var minCoins = 999
         // 遍历每一种面值的硬币，看是否可作为找零的其中之一
-        for kind in 0 ..< values.count {
+        for (i, value) in values.enumerated() {
             // 若当前面值的硬币小于当前的cents则分解问题并查表
-            if values[kind] <= cents {
-                let temp = coinsUsed[cents - values[kind]] + 1
+            if value <= cents {
+                let temp = coinsUsed[cents - value] + 1
                 
                 if (temp < minCoins) {
                     minCoins = temp
-                    last = kind
+                    last = i
                 }
                 
             }
