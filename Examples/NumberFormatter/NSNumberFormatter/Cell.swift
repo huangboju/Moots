d83e3b85d035591462fd0ad810ff1f8f3c876f60
huangboju <bhuang@xiaohongshu.com>
2018-11-01 13:33:43 +0800

    删除没用代码

--- Examples/NumberFormatter/NSNumberFormatter/Cell.swift
+++ Examples/NumberFormatter/NSNumberFormatter/Cell.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class Cell: UITableViewCell {
-    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
         detailTextLabel?.textColor = UIColor.gray
     }
