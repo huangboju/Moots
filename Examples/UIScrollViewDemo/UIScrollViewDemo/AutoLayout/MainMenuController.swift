2a9176dbe008c6fc8124e355bf3dfc46416a716b
huangboju <bhuang@xiaohongshu.com>
2018-11-12 19:27:30 +0800

    整理代码

--- Examples/UIScrollViewDemo/UIScrollViewDemo/AutoLayout/MainMenuController.swift
+++ Examples/UIScrollViewDemo/UIScrollViewDemo/AutoLayout/MainMenuController.swift
@@ -1,58 +1,70 @@
 //
-//  ViewController.swift
-//  RunTime
+//  AutoLayoutMainController.swift
+//  UIScrollViewDemo
 //
-//  Created by 伯驹 黄 on 2016/10/19.
-//  Copyright © 2016年 伯驹 黄. All rights reserved.
+//  Created by 黄伯驹 on 2017/10/21.
+//  Copyright © 2017年 伯驹 黄. All rights reserved.
 //
 
 import UIKit
 
-class CycleViewController: UIViewController {
+class AutoLayoutBaseController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        initSubviews()
+    }
+
+    func initSubviews() {}
+}
+
+class AutoLayoutMainCell: UITableViewCell {
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        separatorInset = .zero
+        preservesSuperviewLayoutMargins = false
+        layoutMargins = .zero
+    }
+}
+
+class MainMenuController: UIViewController {
     fileprivate lazy var tableView: UITableView = {
         let tableView = UITableView(frame: self.view.frame, style: .grouped)
         tableView.dataSource = self
         tableView.delegate = self
         return tableView
     }()
-
+    
     lazy var data: [[UIViewController.Type]] = [
         [
-            SignInController.self,
-            CodeSignInController.self,
-            SignUpController.self,
-            SignUpSuccessController.self,
-            ResetPasswordController.self,
-            VerifyPhoneNumberController.self
+            AutoLayoutController.self,
+            CollectionViewSelfSizing.self,
+            TableViewSelfsizing.self,
+            LayoutTrasition.self,
+            ExpandingCollectionViewController.self,
+            TableViewFooterSelfSizing.self,
+            HiddenLayoutTestController.self,
+            TableNestCollectionController.self,
+            TagsController.self,
+            TextViewSelfsizingController.self,
+            AlignmentRectController.self
         ],
         [
-            VerifyCoInfoSuccessController.self,
-            VerifyCoInfoController.self,
-            NotCompanyUserController.self,
-            MyCompanyController.self,
-            MemberAlertViewController.self
+            AutolayoutMenuController.self
         ]
     ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "CycleViewController"
+        
+        title = "\(classForCoder)"
 
-        let cycleView = CycleView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 130))
-        cycleView.set(contents: ["", "", "", "", ""])
-        tableView.tableHeaderView = cycleView
-
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.register(AutoLayoutMainCell.self, forCellReuseIdentifier: "cell")
         view.addSubview(tableView)
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 }
 
-extension CycleViewController: UITableViewDataSource {
+extension MainMenuController: UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
         return data.count
     }
@@ -66,18 +78,20 @@ extension CycleViewController: UITableViewDataSource {
     }
 }
 
-extension CycleViewController: UITableViewDelegate {
+extension MainMenuController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        cell.accessoryType = .disclosureIndicator
         cell.textLabel?.text = "\(data[indexPath.section][indexPath.row].classForCoder())"
     }
-    
+
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: false)
         let controllerName = "\(data[indexPath.section][indexPath.row].classForCoder())"
         if let controller = controllerName.fromClassName() as? UIViewController {
             controller.title = controllerName
             controller.hidesBottomBarWhenPushed = true
+            controller.view.backgroundColor = .white
             navigationController?.pushViewController(controller, animated: true)
         }
     }
