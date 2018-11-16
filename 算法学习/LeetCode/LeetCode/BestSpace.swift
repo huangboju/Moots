d399c23c882062baef4fe85df2c853a2e37cc539
huangboju <bhuang@xiaohongshu.com>
2018-11-16 11:51:20 +0800

    整理代码

--- 算法学习/LeetCode/LeetCode/BestSpace.swift
+++ 算法学习/LeetCode/LeetCode/BestSpace.swift
@@ -2,30 +2,13 @@
 //  BestSpace.swift
 //  LeetCode
 //
-//  Created by xiAo_Ju on 2018/10/15.
+//  Created by xiAo_Ju on 2018/11/16.
 //  Copyright © 2018 伯驹 黄. All rights reserved.
 //
 
 import Foundation
 
-//func bestSpace(_ a: [Int], target: Int) -> [Int] {
-//    
-//}
 
-func moveZeroes(_ nums: inout [Int]) {
-    var fast = 0
-    var slow = 0
-    let n = nums.count
+func bestSpace(_ a: [Int], target: Int) -> [Int] {
     
-    while fast < n {
-        if nums[fast] != 0 {
-            nums[slow] = nums[fast]
-            slow += 1
-        }
-        fast += 1
-    }
-    for i in slow ..< n {
-        nums[i] = 0
-    }
 }
-
