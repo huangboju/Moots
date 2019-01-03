c840b828c1c2ddb1a7b792b8633f71af6cde3a36
huangboju <bhuang@xiaohongshu.com>
2019-01-03 19:30:20 +0800

    加入BlurController

--- Examples/UIScrollViewDemo/UIScrollViewDemo/Extensions.swift
+++ Examples/UIScrollViewDemo/UIScrollViewDemo/Extensions.swift
@@ -190,3 +190,17 @@ extension UIViewController {
         }
     }
 }
+
+extension UIImage {
+    func blurred(radius: CGFloat) -> UIImage {
+        let ciContext = CIContext(options: nil)
+        guard let cgImage = cgImage else { return self }
+        let inputImage = CIImage(cgImage: cgImage)
+        guard let ciFilter = CIFilter(name: "CIGaussianBlur") else { return self }
+        ciFilter.setValue(inputImage, forKey: kCIInputImageKey)
+        ciFilter.setValue(radius, forKey: "inputRadius")
+        guard let resultImage = ciFilter.value(forKey: kCIOutputImageKey) as? CIImage else { return self }
+        guard let cgImage2 = ciContext.createCGImage(resultImage, from: inputImage.extent) else { return self }
+        return UIImage(cgImage: cgImage2)
+    }
+}
