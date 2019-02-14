e23ba8b50ec6f994c2d79a92319d5364f3d1a62f
huangboju <bhuang@xiaohongshu.com>
2019-02-14 22:31:54 +0800

    加入标题

--- Examples/CoreImageStudy/CoreImageStudy/DisplayView.swift
+++ Examples/CoreImageStudy/CoreImageStudy/DisplayView.swift
@@ -10,17 +10,19 @@ import UIKit
 
 class DisplayView: UIView {
 
-    private lazy var originalImageView: UIImageView = {
-        let imageView = UIImageView()
+    private lazy var originalImageView: ContentView = {
+        let imageView = ContentView()
         imageView.backgroundColor = .red
         imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.text = "Original"
         return imageView
     }()
     
-    private lazy var processedImageView: UIImageView = {
-        let imageView = UIImageView()
+    private lazy var processedImageView: ContentView = {
+        let imageView = ContentView()
         imageView.backgroundColor = .blue
         imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.text = "Processed"
         return imageView
     }()
 
@@ -45,4 +47,53 @@ class DisplayView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
+}
+
+class ContentView: UIView {
+    
+    var image: UIImage? {
+        didSet {
+            imageView.image = image
+        }
+    }
+    
+    var text: String? {
+        didSet {
+            textLabel.text = text
+        }
+    }
+    
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.backgroundColor = UIColor(white: 0.85, alpha: 1)
+        return imageView
+    }()
+    
+    private lazy var textLabel: UILabel = {
+        let textLabel = UILabel()
+        textLabel.font = UIFont.boldSystemFont(ofSize: 48)
+        textLabel.translatesAutoresizingMaskIntoConstraints = false
+        return textLabel
+    }()
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(imageView)
+        addSubview(textLabel)
+        
+        imageView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        imageView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        imageView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        
+        textLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 8).isActive = true
+        textLabel.centerXAnchor.constraint(equalTo: imageView.centerXAnchor).isActive = true
+        textLabel.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
