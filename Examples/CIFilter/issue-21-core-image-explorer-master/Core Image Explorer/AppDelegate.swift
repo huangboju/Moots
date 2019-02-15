d01806b8a581bdc7e3a75f6ec10e77d579a1b24e
huangboju <bhuang@xiaohongshu.com>
2019-02-15 19:38:46 +0800

    加入filter

---
+++ Examples/CIFilter/issue-21-core-image-explorer-master/Core Image Explorer/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Core Image Explorer
+//
+//  Created by Warren Moore on 1/6/15.
+//  Copyright (c) 2015 objc.io. All rights reserved.
+//
+
+import UIKit
+
+let kSampleImageName = "duckling.jpg"
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
+
