6631b8471e43b29ba87a8b4693eb1b6b01c95f80
huangboju <bhuang@xiaohongshu.com>
2019-02-15 16:22:55 +0800

    加入转场

--- Examples/CoreImageStudy/CoreImageStudy/LandscapeNav.swift
+++ Examples/CoreImageStudy/CoreImageStudy/LandscapeNav.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class LandscapeNav: UINavigationController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        modalPresentationStyle = .fullScreen
+        transitioningDelegate = self
+    }
 
     override var shouldAutorotate: Bool {
         return true
@@ -16,5 +22,15 @@ class LandscapeNav: UINavigationController {
     
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         return .landscapeRight
+    }
+}
+
+extension LandscapeNav: UIViewControllerTransitioningDelegate {
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return LandscapeTransitionAnimator(targetEdge: .top)
+    }
+
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return LandscapeTransitionAnimator(targetEdge: .left)
     }
 }
