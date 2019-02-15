6aa29a9d732dfb2a21056be55b03c53faee7a267
huangboju <bhuang@xiaohongshu.com>
2019-02-15 20:42:00 +0800

    init DynamicsCatalog

---
+++ Examples/DynamicsCatalog/DynamicsCatalog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DynamicsCatalog
+//
+//  Created by 黄伯驹 on 2019/2/15.
+//  Copyright © 2019 黄伯驹. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
