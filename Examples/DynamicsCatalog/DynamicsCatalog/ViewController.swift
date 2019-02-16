88adc28a00e770ad37cef08f4111f276528d2c1d
huangboju <bhuang@xiaohongshu.com>
2019-02-16 10:18:43 +0800

    加入gravity

--- Examples/DynamicsCatalog/DynamicsCatalog/ViewController.swift
+++ Examples/DynamicsCatalog/DynamicsCatalog/ViewController.swift
@@ -9,12 +9,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    fileprivate lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: self.view.frame, style: .grouped)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+    
+    lazy var data: [[UIViewController.Type]] = [
+        [
+            GravityVC.self
+        ]
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        title = "Dynamics"
+
+        view.addSubview(tableView)
     }
+}
 
+extension ViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data[section].count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+    }
+}
 
+extension ViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        cell.accessoryType = .disclosureIndicator
+        cell.textLabel?.text = "\(data[indexPath.section][indexPath.row])"
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        defer { tableView.deselectRow(at: indexPath, animated: false) }
+        let vcClass = data[indexPath.section][indexPath.row]
+        let controller = vcClass.init()
+        controller.title = vcClass.description()
+        controller.hidesBottomBarWhenPushed = true
+        show(controller, sender: nil)
+    }
 }
 
