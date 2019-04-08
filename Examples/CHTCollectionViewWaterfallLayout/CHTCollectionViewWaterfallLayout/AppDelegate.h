4bca765338f3402b97d993b1397131d9805916aa
huangboju <bhuang@xiaohongshu.com>
2019-04-08 20:07:16 +0800

    加入layout

---
+++ Examples/CHTCollectionViewWaterfallLayout/CHTCollectionViewWaterfallLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CHTCollectionViewWaterfallLayout
+//
+//  Created by xiAo_Ju on 2019/4/8.
+//  Copyright © 2019 黄伯驹. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Examples/CHTCollectionViewWaterfallLayout/CHTCollectionViewWaterfallLayout/CHTCollectionViewWaterfallCell.h
@@ -0,0 +1,13 @@
+//
+//  UICollectionViewWaterfallCell.h
+//  Demo
+//
+//  Created by Nelson on 12/11/27.
+//  Copyright (c) 2012年 Nelson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CHTCollectionViewWaterfallCell : UICollectionViewCell
+@property (nonatomic, strong) UIImageView *imageView;
+@end
