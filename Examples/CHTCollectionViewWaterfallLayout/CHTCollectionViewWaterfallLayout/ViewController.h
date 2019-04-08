f0979476a692afe080fb142ed8e23bcb6a06a050
huangboju <bhuang@xiaohongshu.com>
2019-04-08 20:07:16 +0800

    加入layout

---
+++ Examples/CHTCollectionViewWaterfallLayout/CHTCollectionViewWaterfallLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CHTCollectionViewWaterfallLayout
+//
+//  Created by xiAo_Ju on 2019/4/8.
+//  Copyright © 2019 黄伯驹. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Examples/CHTCollectionViewWaterfallLayout/CHTCollectionViewWaterfallLayout/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CHTCollectionViewWaterfallLayout
+//
+//  Created by xiAo_Ju on 2019/4/8.
+//  Copyright © 2019 黄伯驹. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
