24ecf1b7d205dedb0775208b8e5bb94b5311abef
huangboju <bhuang@xiaohongshu.com>
2019-04-08 20:07:16 +0800

    加入layout

---
+++ Examples/CHTCollectionViewWaterfallLayout/CHTCollectionViewWaterfallLayout/CHTCollectionViewWaterfallFooter.m
@@ -0,0 +1,21 @@
+//
+//  CHTCollectionViewWaterfallFooter.m
+//  Demo
+//
+//  Created by Neil Kimmett on 28/10/2013.
+//  Copyright (c) 2013 Nelson. All rights reserved.
+//
+
+#import "CHTCollectionViewWaterfallFooter.h"
+
+@implementation CHTCollectionViewWaterfallFooter
+
+#pragma mark - Accessors
+- (id)initWithFrame:(CGRect)frame {
+  if (self = [super initWithFrame:frame]) {
+    self.backgroundColor = [UIColor blueColor];
+  }
+  return self;
+}
+
+@end
