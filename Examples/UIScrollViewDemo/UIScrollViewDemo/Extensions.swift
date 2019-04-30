5a72cf43afbb066ce1a4173115e3e802a339f884
huangboju <bhuang@xiaohongshu.com>
2019-04-30 14:28:43 +0800

    Update Extensions.swift

--- Examples/UIScrollViewDemo/UIScrollViewDemo/Extensions.swift
+++ Examples/UIScrollViewDemo/UIScrollViewDemo/Extensions.swift
@@ -98,6 +98,10 @@ extension UIColor {
         UIGraphicsEndImageContext()
         return resultImage!
     }
+
+    static var random: UIColor {
+        return UIColor(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1), alpha: 1.0)
+    }
 }
 
 extension UITableView {
