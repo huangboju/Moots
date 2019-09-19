f13895c4d3dcc405b4184ec5f282c052cb5cce72
huangboju <bhuang@xiaohongshu.com>
2019-09-19 10:08:17 +0800

    加入新算法

--- 算法学习/LeetCode/LeetCode/MaxSlidingWindow.swift
+++ 算法学习/LeetCode/LeetCode/MaxSlidingWindow.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 
+// https://leetcode-cn.com/explore/interview/card/top-interview-quesitons-in-2018/266/heap-stack-queue/1158/
 //输入: nums = [1,3,-1,-3,5,3,6,7], 和 k = 3
 //输出: [3,3,5,5,6,7]
 //解释:
--- 算法学习/LeetCode/LeetCode/MinSubArrayLen.swift
+++ 算法学习/LeetCode/LeetCode/MinSubArrayLen.swift
@@ -9,9 +9,10 @@
 import Foundation
 
 /// https://leetcode-cn.com/explore/featured/card/all-about-array/233/sliding-window/971/
-///输入: s = 7, nums = [2,3,1,2,4,3]
-///输出: 2
-///解释: 子数组 [4,3] 是该条件下的长度最小的连续子数组。
+/// 给定一个含有 n 个正整数的数组和一个正整数 s ，找出该数组中满足其和 ≥ s 的长度最小的连续子数组。如果不存在符合条件的连续子数组，返回 0。
+/// 输入: s = 7, nums = [2,3,1,2,4,3]
+/// 输出: 2
+/// 解释: 子数组 [4,3] 是该条件下的长度最小的连续子数组。
 func minSubArrayLen(s: Int, nums: [Int]) -> Int {
     var sum = 0
     var j = 0
