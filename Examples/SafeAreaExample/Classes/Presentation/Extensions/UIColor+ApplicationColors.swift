afd7ea7a0bf980c58f6331d283cf9fa83a974120
huangboju <bhuang@xiaohongshu.com>
2019-12-30 17:27:33 +0800

    精华代码

---
+++ Examples/SafeAreaExample/Classes/Presentation/Extensions/UIColor+ApplicationColors.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor+ApplicationColors.swift
+//  SafeAreaExample
+//
+//  Created by Evgeny Mikhaylov on 17/10/2017.
+//  Copyright © 2017 Rosberry. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static var grayBackgroundColor: UIColor {
+        return UIColor(red: 222.0 / 255.0, green: 222.0 / 255.0, blue: 222.0 / 255.0, alpha: 1.0)
+    }
+}
