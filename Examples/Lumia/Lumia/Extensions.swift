af73d27ea4b5e264dc69c77815ca9e36dc74fa5e
huangboju <bhuang@xiaohongshu.com>
2019-12-31 16:21:59 +0800

    加入动画

---
+++ Examples/Lumia/Lumia/Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  Extensions.swift
+//  Lumia
+//
+//  Created by xiAo_Ju on 2019/12/31.
+//  Copyright © 2019 黄伯驹. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    convenience init(hex: Int, alpha: CGFloat = 1) {
+        let r = CGFloat((hex & 0xFF0000) >> 16) / 255
+        let g = CGFloat((hex & 0xFF00) >> 8) / 255
+        let b = CGFloat((hex & 0xFF)) / 255
+        self.init(red: r, green: g, blue: b, alpha: alpha)
+    }
+}
