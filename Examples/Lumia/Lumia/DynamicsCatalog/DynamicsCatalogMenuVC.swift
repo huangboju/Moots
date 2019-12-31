4a8ddb605266f4fbc591372b1c1bf225299c20be
huangboju <bhuang@xiaohongshu.com>
2019-12-31 21:32:14 +0800

    加入push

--- Examples/Lumia/Lumia/DynamicsCatalog/DynamicsCatalogMenuVC.swift
+++ Examples/Lumia/Lumia/DynamicsCatalog/DynamicsCatalogMenuVC.swift
@@ -18,8 +18,8 @@ class DynamicsCatalogMenuVC: ListVC {
             Row<FluidInterfacesCell>(viewData: Interface(name: "Collision + Gravity", segue: .segue(CollisionGravityVC.self))),
             Row<FluidInterfacesCell>(viewData: Interface(name: "Attachments + Gravity", segue: .segue(AttachmentsVC.self))),
             Row<FluidInterfacesCell>(viewData: Interface(name: "Collision + Gravity + Spring", segue: .segue(CollisionsGravitySpringVC.self))),
-            Row<FluidInterfacesCell>(viewData: Interface(name: "Snap", segue: .segue(SnapVC.self)))
-            
+            Row<FluidInterfacesCell>(viewData: Interface(name: "Snap", segue: .segue(SnapVC.self))),
+            Row<FluidInterfacesCell>(viewData: Interface(name: "Instantaneous Push + Collision", segue: .segue(InstantaneousPushVC.self)))
         ]
     }
 }
