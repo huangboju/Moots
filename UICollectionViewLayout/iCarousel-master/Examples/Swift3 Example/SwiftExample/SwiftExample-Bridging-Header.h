94afb81eeebfc3791d21e814b226d4099c2884ec
huangboju <bhuang@xiaohongshu.com>
2019-12-31 11:18:27 +0800

    删除没有代码

--- UICollectionViewLayout/iCarousel-master/Examples/Swift3 Example/SwiftExample/SwiftExample-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  SwiftExample-Bridging-Header.h
-//  SwiftExample
-//
-//  Created by Nick Lockwood on 30/07/2014.
-//  Copyright (c) 2014 Charcoal Design. All rights reserved.
-//
-
-#import "iCarousel.h"
