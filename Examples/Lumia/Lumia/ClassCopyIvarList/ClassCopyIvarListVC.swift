85482c6b796d3a1db2f2bcfbcdc9c7e88d07d793
huangboju <bhuang@xiaohongshu.com>
2019-12-31 17:30:09 +0800

    删除没用代码

--- Examples/Lumia/Lumia/ClassCopyIvarList/ClassCopyIvarListVC.swift
+++ Examples/Lumia/Lumia/ClassCopyIvarList/ClassCopyIvarListVC.swift
@@ -8,79 +8,31 @@
 
 import UIKit
 
-class ClassCopyIvarListVC: UIViewController {
-    lazy var rows: [RowType] = [
-        Row<FluidInterfacesCell>(viewData: Interface(name: "UIPickerView", segue: .segue(ClassCopyIvarDetailVC.self))),
-        Row<FluidInterfacesCell>(viewData: Interface(name: "UIDatePicker", segue: .segue(ClassCopyIvarDetailVC.self))),
-        Row<FluidInterfacesCell>(viewData: Interface(name: "UITextField", segue: .segue(ClassCopyIvarDetailVC.self))),
-        Row<FluidInterfacesCell>(viewData: Interface(name: "UISlider", segue: .segue(ClassCopyIvarDetailVC.self))),
-        Row<FluidInterfacesCell>(viewData: Interface(name: "UIImageView", segue: .segue(ClassCopyIvarDetailVC.self))),
-        Row<FluidInterfacesCell>(viewData: Interface(name: "UITabBarItem", segue: .segue(ClassCopyIvarDetailVC.self))),
-        Row<FluidInterfacesCell>(viewData: Interface(name: "UIActivityIndicatorView", segue: .segue(ClassCopyIvarDetailVC.self))),
-        Row<FluidInterfacesCell>(viewData: Interface(name: "UIBarButtonItem", segue: .segue(ClassCopyIvarDetailVC.self))),
-        Row<FluidInterfacesCell>(viewData: Interface(name: "UILabel", segue: .segue(ClassCopyIvarDetailVC.self))),
-        Row<FluidInterfacesCell>(viewData: Interface(name: "UINavigationBar", segue: .segue(ClassCopyIvarDetailVC.self))),
-        Row<FluidInterfacesCell>(viewData: Interface(name: "UIProgressView", segue: .segue(ClassCopyIvarDetailVC.self))),
-        Row<FluidInterfacesCell>(viewData: Interface(name: "UIAlertAction", segue: .segue(ClassCopyIvarDetailVC.self))),
-        Row<FluidInterfacesCell>(viewData: Interface(name: "UIViewController", segue: .segue(ClassCopyIvarDetailVC.self))),
-        Row<FluidInterfacesCell>(viewData: Interface(name: "UIRefreshControl", segue: .segue(ClassCopyIvarDetailVC.self))),
-        Row<FluidInterfacesCell>(viewData: Interface(name: "UIAlertController", segue: .segue(ClassCopyIvarDetailVC.self))),
-    ]
-
-    private lazy var collectionView: UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
-        layout.sectionInset = UIEdgeInsets(top: 14, left: 0, bottom: 14, right: 0)
-        layout.minimumLineSpacing = 10
-        layout.minimumInteritemSpacing = 4
-        layout.itemSize = CGSize(width: self.view.frame.width, height: 60)
-        let collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
-        collectionView.backgroundColor = UIColor(white: 0.05, alpha: 1)
-        collectionView.dataSource = self
-        collectionView.delegate = self
-        collectionView.register(FluidInterfacesCell.self, forCellWithReuseIdentifier: "cellId")
-        collectionView.translatesAutoresizingMaskIntoConstraints = false
-        return collectionView
-    }()
-
+class ClassCopyIvarListVC: ListVC {
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        title = "Fluid Interfaces"
-        
-        view.backgroundColor = UIColor(white: 0.05, alpha: 1)
+        title = "属性查看器"
 
-        view.addSubview(collectionView)
-        collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        collectionView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        collectionView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        collectionView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        rows = [
+            Row<FluidInterfacesCell>(viewData: Interface(name: "UIPickerView", segue: .segue(ClassCopyIvarDetailVC.self))),
+            Row<FluidInterfacesCell>(viewData: Interface(name: "UIDatePicker", segue: .segue(ClassCopyIvarDetailVC.self))),
+            Row<FluidInterfacesCell>(viewData: Interface(name: "UITextField", segue: .segue(ClassCopyIvarDetailVC.self))),
+            Row<FluidInterfacesCell>(viewData: Interface(name: "UISlider", segue: .segue(ClassCopyIvarDetailVC.self))),
+            Row<FluidInterfacesCell>(viewData: Interface(name: "UIImageView", segue: .segue(ClassCopyIvarDetailVC.self))),
+            Row<FluidInterfacesCell>(viewData: Interface(name: "UITabBarItem", segue: .segue(ClassCopyIvarDetailVC.self))),
+            Row<FluidInterfacesCell>(viewData: Interface(name: "UIActivityIndicatorView", segue: .segue(ClassCopyIvarDetailVC.self))),
+            Row<FluidInterfacesCell>(viewData: Interface(name: "UIBarButtonItem", segue: .segue(ClassCopyIvarDetailVC.self))),
+            Row<FluidInterfacesCell>(viewData: Interface(name: "UILabel", segue: .segue(ClassCopyIvarDetailVC.self))),
+            Row<FluidInterfacesCell>(viewData: Interface(name: "UINavigationBar", segue: .segue(ClassCopyIvarDetailVC.self))),
+            Row<FluidInterfacesCell>(viewData: Interface(name: "UIProgressView", segue: .segue(ClassCopyIvarDetailVC.self))),
+            Row<FluidInterfacesCell>(viewData: Interface(name: "UIAlertAction", segue: .segue(ClassCopyIvarDetailVC.self))),
+            Row<FluidInterfacesCell>(viewData: Interface(name: "UIViewController", segue: .segue(ClassCopyIvarDetailVC.self))),
+            Row<FluidInterfacesCell>(viewData: Interface(name: "UIRefreshControl", segue: .segue(ClassCopyIvarDetailVC.self))),
+            Row<FluidInterfacesCell>(viewData: Interface(name: "UIAlertController", segue: .segue(ClassCopyIvarDetailVC.self))),
+        ]
     }
 
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        collectionView.visibleCells.forEach {
-            $0.isHighlighted = false
-        }
-        
-    }
-}
-
-extension ClassCopyIvarListVC: UICollectionViewDataSource {
-
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return rows.count
-    }
-
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
-        rows[indexPath.row].update(cell: cell)
-        return cell
-    }
-}
-
-extension ClassCopyIvarListVC: UICollectionViewDelegate {
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let item: Interface = rows[indexPath.row].cellItem()
         guard let segue = item.segue else { return }
         show(segue) { (vc: ClassCopyIvarDetailVC) in
@@ -92,7 +44,7 @@ extension ClassCopyIvarListVC: UICollectionViewDelegate {
 
 
 
-class ClassCopyIvarDetailVC: UIViewController {
+class ClassCopyIvarDetailVC: ListVC {
     
     var className: String? {
         didSet {
@@ -122,48 +74,5 @@ class ClassCopyIvarDetailVC: UIViewController {
         }
 
         return classArray
-    }
-
-    lazy var rows: [RowType] = []
-    
-    private lazy var collectionView: UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
-        layout.sectionInset = UIEdgeInsets(top: 14, left: 0, bottom: 14, right: 0)
-        layout.minimumLineSpacing = 10
-        layout.minimumInteritemSpacing = 4
-        layout.itemSize = CGSize(width: self.view.frame.width, height: 60)
-        let collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
-        collectionView.backgroundColor = UIColor(white: 0.05, alpha: 1)
-        collectionView.dataSource = self
-        collectionView.register(FluidInterfacesCell.self, forCellWithReuseIdentifier: "cellId")
-        collectionView.translatesAutoresizingMaskIntoConstraints = false
-        return collectionView
-    }()
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        title = "Fluid Interfaces"
-        
-        view.backgroundColor = UIColor(white: 0.05, alpha: 1)
-
-        view.addSubview(collectionView)
-        collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        collectionView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        collectionView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        collectionView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-    }
-}
-
-extension ClassCopyIvarDetailVC: UICollectionViewDataSource {
-
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return rows.count
-    }
-
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
-        rows[indexPath.row].update(cell: cell)
-        return cell
     }
 }
