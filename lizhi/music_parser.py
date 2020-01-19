961b4a29f07ec47d9f5564410bb6a201c21faf6b
huangboju <bhuang@xiaohongshu.com>
2020-01-19 14:22:17 +0800

    加入李志

---
+++ lizhi/music_parser.py
@@ -0,0 +1,34 @@
+from bs4 import BeautifulSoup
+import os
+import urllib.request
+
+def create_dict(dict_name):
+    try:
+        os.makedirs(dict_name)
+    except FileExistsError:
+        # directory already exists
+        pass
+
+def parser_html():
+
+    create_dict('lizhi')
+
+    with open('lizhi.html', 'rt') as f:
+        soup = BeautifulSoup(f, "html.parser")
+
+        for link in soup.find_all('a'):
+            albumname = link.get('albumname')
+            if albumname == None:
+                music_name = link.string
+                music_link = link.get('hrefsrc')
+                print(music_link)
+                filedata = urllib.request.urlopen(music_link)
+                datatowrite = filedata.read()
+
+                with open('/Users/huangboju/Desktop/lizhi/lizhi' + music_name + '.mp3', 'wb') as music:
+                    music.write(datatowrite)
+                break
+            else:
+                create_dict('lizhi/' + albumname)
+
+parser_html()
