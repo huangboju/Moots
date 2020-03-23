e220fc51a29d1dce229914741f385938158ff8bf
huangboju <bhuang@xiaohongshu.com>
2020-03-23 19:48:51 +0800

    整理代码

--- 算法学习/LeetCode/LeetCode/LongestSubstringWithoutRepeatingCharacters.swift
+++ 算法学习/LeetCode/LeetCode/LongestSubstringWithoutRepeatingCharacters.swift
@@ -12,16 +12,17 @@ import Foundation
 // "abcabcbb"
 
 func lengthOfLongestSubstring(_ s: String) -> Int {
-
+    
     guard s.count > 1 else { return s.count }
+
     let charArr: [Character] = Array(s)
     
     var maxLen = 1
     var tempLen = 1
     var hashMap = [charArr[0]: 0]
-
-    for (i, char) in charArr.enumerated() {
-        if let lastPosion = hashMap[char] {
+    
+    for i in 1 ..< s.count {
+        if let lastPosion = hashMap[charArr[i]] {
             if tempLen < i - lastPosion { // 这种情况 "ppwwpwkew"
                 tempLen += 1
             } else {
@@ -31,7 +32,7 @@ func lengthOfLongestSubstring(_ s: String) -> Int {
             tempLen += 1
         }
 
-        hashMap[char] = i
+        hashMap[charArr[i]] = i
 
         maxLen = max(tempLen, maxLen)
     }
