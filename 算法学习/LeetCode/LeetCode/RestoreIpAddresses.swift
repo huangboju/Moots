cdc6bf1a43792d4b7206ea389d16a85674ffa413
huangboju <xiaoju.foxmail@gmail.com>
2020-03-28 21:28:12 +0800

    加入新算法

--- 算法学习/LeetCode/LeetCode/RestoreIpAddresses.swift
+++ 算法学习/LeetCode/LeetCode/RestoreIpAddresses.swift
@@ -9,41 +9,39 @@
 import Foundation
 
 func restoreIpAddresses(_ s: String) -> [String] {
-    if s.count < 4 || s.count > 12 {
-        return []
-    }
-
-    let characters: [Character] = Array(s)
-    var result = [String]()
-    var candidate = [String]()
-
-    backtracking(characters, 0, &candidate, &result)
-
+    if s.count < 4 || s.count > 12 { return [] }
+    
+    let chars = Array(s)
+    var result: [String]  = []
+    var candidate: [String] = []
+    
+    backTracking(chars, pos: 0, candidate: &candidate, result: &result)
+    
     return result
 }
-private func backtracking(_ characters: [Character], _ pos: Int, _ candidate: inout [String], _ result: inout [String]) {
+
+private func backTracking(_ chars:[String.Element], pos: Int, candidate: inout [String], result: inout [String]) {
     if candidate.count == 4 {
         result.append(candidate.joined(separator: "."))
         return
     }
-
-    let charsLeft = characters.count - pos
+    
+    let charsLeft = chars.count - pos
     let groupsLeft = 4 - candidate.count
-    let minLen = groupsLeft == 1 ? charsLeft - groupsLeft + 1 : 1
-    let maxLen = characters[pos] == "0" ? 1 : min(3, charsLeft - groupsLeft + 1)
-
+    let minLen = groupsLeft == 1 ? charsLeft : 1
+    let maxLen = chars[pos] == "0" ? 1 : min(3, charsLeft - groupsLeft + 1)
+    
     if minLen > maxLen {
         return
     }
-
+    
     for len in minLen...maxLen {
-        let num = String(characters[pos..<(pos + len)])
-        if Int(num)! > 255 {
-            continue
-        }
-        candidate.append(num)
-        backtracking(characters, pos + len, &candidate, &result)
+        let n = String(chars[pos..<(pos + len)])
+        if Int(n)! > 255 { continue }
+        candidate.append(n)
+        backTracking(chars, pos: pos + len, candidate: &candidate, result: &result)
         candidate.removeLast()
     }
 }
+
 
