c201a272b366a0d551322c3a15d3d41576a73fce
huangboju <xiaoju.foxmail@gmail.com>
2020-04-05 21:53:34 +0800

    整理代码

--- 算法学习/LeetCode/LeetCode/MaxAreaOfIsland.swift
+++ 算法学习/LeetCode/LeetCode/MaxAreaOfIsland.swift
@@ -9,33 +9,30 @@
 import Foundation
 
 func maxAreaOfIsland(_ grid: [[Int]]) -> Int {
-    if grid.isEmpty {
-        return 0
-    }
+    if grid.isEmpty { return 0 }
     let row = grid.count
     let col = grid[0].count
     var res = 0
-    let direction = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+    let direction = [(1, 0), (-1, 0), (0, 1), (0, -1)]
     var visited = [[Int]](repeating: [Int](repeating: 0, count: col), count: row)
-    for i in 0..<row {
-        for j in 0..<col {
-            if grid[i][j] == 1 {
-                res = max(res, dfs(grid, &visited, direction, i, j))
+    for x in 0..<row {
+        for y in 0..<col {
+            if grid[x][y] == 1 {
+                res = max(res, dfs(grid, &visited, direction, x, y))
             }
         }
     }
     return res
 }
-func dfs(_ grid: [[Int]], _ visited: inout [[Int]], _ direction: [[Int]], _ i : Int, _ j : Int) -> Int {
-    visited[i][j] = 1
+func dfs(_ grid: [[Int]], _ visited: inout [[Int]], _ direction: [(Int, Int)], _ x : Int, _ y : Int) -> Int {
+    visited[x][y] = 1
     var area = 1
     for k in 0..<4 {
-        let newRow = i + direction[k][0]
-        let newCol = j + direction[k][1]
-        if newRow >= 0 && newRow < grid.count && newCol >= 0 && newCol < grid[0].count
-            && grid[newRow][newCol] == 1 && visited[newRow][newCol] != 1 {
-            area += dfs(grid, &visited, direction, newRow, newCol)
+        let dx = x + direction[k].0
+        let dy = y + direction[k].1
+        if 0..<grid.count ~= dx, 0..<grid[0].count ~= dy, grid[dx][dy] == 1, visited[dx][dy] != 1 {
+            area += dfs(grid, &visited, direction, dx, dy)
         }
     }
-        return area
+    return area
 }
