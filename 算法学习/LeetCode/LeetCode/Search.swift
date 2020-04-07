eb74b82fb4710758a57e8c734763519def5bfcd0
huangboju <xiaoju.foxmail@gmail.com>
2020-04-07 09:48:33 +0800

    增加代码

--- 算法学习/LeetCode/LeetCode/Search.swift
+++ 算法学习/LeetCode/LeetCode/Search.swift
@@ -35,3 +35,32 @@ func search(_ nums: [Int], _ target: Int) -> Int {
     }
     return -1
 }
+
+func _search(_ nums: [Int], target: Int) -> Int {
+    if nums.isEmpty { return -1 }
+    var start = 0
+    var end = nums.count - 1
+    var mid = 0
+    
+    while start <= end {
+        mid = start + (end - start) / 2
+        if nums[mid] == target { return mid }
+        if nums[start] <= target {
+            if nums[start] == target { return start }
+            if target > nums[start] && target < nums[mid] {
+                end = mid - 1
+            } else {
+                start = mid + 1
+            }
+        } else {
+            if nums[end] == target { return start }
+            if target > nums[mid] && target < nums[end] {
+                start = mid + 1
+            } else {
+                end = mid - 1
+            }
+        }
+    }
+    
+    return -1
+}
