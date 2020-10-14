1d26fd3478bcfab3d37e59018169df4e0154996f
huangboju <xiaoju.foxmail@gmail.com>
2020-10-14 21:31:59 +0800

    解决编译问题

--- Examples/UIScrollViewDemo/UIScrollViewDemo/Controllers/LazyLoadVC/GLImageCell.swift
+++ Examples/UIScrollViewDemo/UIScrollViewDemo/Controllers/LazyLoadVC/GLImageCell.swift
@@ -16,7 +16,7 @@ class GLImageCell: UITableViewCell {
         return photoView
     }()
 
-    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         contentView.addSubview(photoView)
     }
