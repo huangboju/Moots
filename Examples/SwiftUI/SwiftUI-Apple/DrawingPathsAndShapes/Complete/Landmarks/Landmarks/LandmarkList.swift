5c0eb4759c4db9b6c1a2f463fb956a7b1d421b55
huangboju <xiaoju.foxmail@gmail.com>
2020-11-21 15:25:52 +0800

    加入apple demo

---
+++ Examples/SwiftUI/SwiftUI-Apple/DrawingPathsAndShapes/Complete/Landmarks/Landmarks/LandmarkList.swift
@@ -0,0 +1,45 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+A view showing a list of landmarks.
+*/
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject private var userData: UserData
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Toggle(isOn: $userData.showFavoritesOnly) {
+                    Text("Show Favorites Only")
+                }
+                
+                ForEach(userData.landmarks) { landmark in
+                    if !self.userData.showFavoritesOnly || landmark.isFavorite {
+                        NavigationLink(
+                            destination: LandmarkDetail(landmark: landmark)
+                                .environmentObject(self.userData)
+                        ) {
+                            LandmarkRow(landmark: landmark)
+                        }
+                    }
+                }
+            }
+            .navigationBarTitle(Text("Landmarks"))
+        }
+    }
+}
+
+struct LandmarksList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone SE", "iPhone XS Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+        .environmentObject(UserData())
+    }
+}
