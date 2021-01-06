c877407cb7b7d0d2fa95e51332d8ed84de685dc6
huangboju <xiaoju.foxmail@gmail.com>
2021-01-06 20:45:40 +0800

    加入二叉树相关代码

--- 算法学习/LeetCode/LeetCode/IsSymmetric.swift
+++ 算法学习/LeetCode/LeetCode/IsSymmetric.swift
@@ -48,7 +48,7 @@ class SolutionSymmetric {
         level(root, 0, &res)
         return res
     }
-
+    
     func level(_ t: TreeNode?, _ l: Int, _ res: inout [[Int]]) {
         if res.count == l { res.append([]) }
         if let v = t?.val {
@@ -61,37 +61,37 @@ class SolutionSymmetric {
             level(right, l + 1, &res)
         }
     }
-
-// BFS:
-//     func levelOrder(_ root: TreeNode?) -> [[Int]] {
-//        guard let root = root else { return [] }
-//        var result = [[Int]]()
-//        var queue = [root]
-//
-//        while !queue.isEmpty {
-//            var row: [Int] = []
-//            for _ in 0 ..< queue.count {
-//                let first = queue.removeFirst()
-//                row.append(first.val)
-//                if let left = first.left {
-//                    queue.append(left)
-//                }
-//                if let right = first.right {
-//                    queue.append(right)
-//                }
-//            }
-//            result.append(row)
-//        }
-//
-//        return result
-//     }
+    
+    // BFS:
+    //     func levelOrder(_ root: TreeNode?) -> [[Int]] {
+    //        guard let root = root else { return [] }
+    //        var result = [[Int]]()
+    //        var queue = [root]
+    //
+    //        while !queue.isEmpty {
+    //            var row: [Int] = []
+    //            for _ in 0 ..< queue.count {
+    //                let first = queue.removeFirst()
+    //                row.append(first.val)
+    //                if let left = first.left {
+    //                    queue.append(left)
+    //                }
+    //                if let right = first.right {
+    //                    queue.append(right)
+    //                }
+    //            }
+    //            result.append(row)
+    //        }
+    //
+    //        return result
+    //     }
     
     // https://leetcode-cn.com/problems/binary-tree-zigzag-level-order-traversal/
     func zigzagLevelOrder(_ root: TreeNode?) -> [[Int]] {
         guard let root = root else { return [] }
         var result = [[Int]]()
         var queue = [root]
-
+        
         while !queue.isEmpty {
             var row: [Int] = []
             for _ in 0 ..< queue.count {
@@ -111,4 +111,38 @@ class SolutionSymmetric {
         }
         return result
     }
- }
+    
+    func inorderTraversal(_ root: TreeNode?) -> [Int] {
+        var node = root
+        var stack: [TreeNode] = []
+        var result: [Int] = []
+        while true {
+            if node != nil {
+                stack.append(node!)
+                node = node!.left
+            } else if stack.isEmpty {
+                return result
+            } else {
+                node = stack.popLast()
+                result.append(node!.val)
+                node = node?.right
+            }
+        }
+        return result
+    }
+    
+//    func inorderTraversal(_ root: TreeNode?) -> [Int] {
+//        var result: [Int] = []
+//        inorder(root, &result)
+//        return result
+//    }
+//
+//    func inorder(_ root: TreeNode?, _ result: inout [Int]) {
+//        guard let root = root else {
+//            return
+//        }
+//        inorder(root.left, &result)
+//        result.append(root.val)
+//        inorder(root.right, &result)
+//    }
+}
