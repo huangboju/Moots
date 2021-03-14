b5d273685ae2235b9be64432b8c38017dd3c46f1
huangboju <xiaoju.foxmail@gmail.com>
2021-03-14 15:17:39 +0800

    Update EggDrop.swift

--- 算法学习/LeetCode/LeetCode/EggDrop.swift
+++ 算法学习/LeetCode/LeetCode/EggDrop.swift
@@ -8,13 +8,16 @@
 
 import Foundation
 
+// https://leetcode-cn.com/problems/super-egg-drop/?utm_source=LCUS&utm_medium=ip_redirect&utm_campaign=transfer2china
 func superEggDrop(_ K: Int, _ N: Int) -> Int {
+    
+    // 换个思路，需要扔多少个鸡蛋可以覆盖N层楼
     var result: [Int] = Array(repeating: 0, count: K + 1)
     var m = 0
     
     while result[K] < N {
         m += 1
-        for k in (1 ..< K + 1).reversed() {
+        for k in (1 ... K).reversed() {
             result[k] += result[k - 1] + 1
         }
     }
