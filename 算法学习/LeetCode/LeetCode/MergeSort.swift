c1e4143f7a3c6ed4635c441a7c79db7ce21e372a
huangboju <xiaoju.foxmail@gmail.com>
2021-03-14 15:30:23 +0800

    Update MergeSort.swift

--- 算法学习/LeetCode/LeetCode/MergeSort.swift
+++ 算法学习/LeetCode/LeetCode/MergeSort.swift
@@ -18,15 +18,24 @@ import Foundation
 
 
 func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
-    var i = m - 1, j = n - 1
-    while i >= 0 || j >= 0 {
-        if j < 0 || (i >= 0 && nums1[i] > nums2[j]) {
-            nums1[i + j + 1] = nums1[i]
-            i -= 1
+    var index = m+n-1
+    var i = m-1
+    var j = n-1
+    //! 因为是把 nums2 移到 nums1，那么 j = 0 就是终止条件
+    while j >= 0 {
+        if i >= 0 {
+            if nums2[j] >= nums1[i]  {
+                nums1[index] = nums2[j]
+                j -= 1
+            } else {
+                nums1[index] = nums1[i]
+                i -= 1
+            }
         } else {
-            nums1[i + j + 1] = nums2[j]
+            nums1[index] = nums2[j]
             j -= 1
         }
+        index -= 1
     }
 }
 
