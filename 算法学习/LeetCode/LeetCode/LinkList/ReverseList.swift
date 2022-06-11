4ceba632e3aa3d54eab79731aad5fab0ef01f455
xiAo-Ju <xiao-ju@foxmail.com>
2022-06-11 09:15:21 +0800

    ReverseBetween

--- 算法学习/LeetCode/LeetCode/LinkList/ReverseList.swift
+++ 算法学习/LeetCode/LeetCode/LinkList/ReverseList.swift
@@ -22,3 +22,44 @@ func reverseList(_ head: ListNode?) -> ListNode? {
 
     return result
 }
+
+class ReverseBetween {
+    func reverseBetween(_ head: ListNode?, _ left: Int, _ right: Int) -> ListNode? {
+        let dummy = ListNode(-1)
+        dummy.next = head
+        var node: ListNode? = dummy
+
+        for _ in 0 ..< left - 1 {
+            node = node?.next
+        }
+        let firstNode = node
+        let midNode = firstNode?.next
+
+        for _ in 0 ... right - left {
+            node = node?.next
+        }
+        let thirdNode = node?.next
+
+        firstNode?.next = nil
+        node?.next = nil
+
+        _ = reverseLink(midNode)
+
+        firstNode?.next = node
+        midNode?.next = thirdNode
+
+        return dummy.next
+    }
+
+    func reverseLink(_ head: ListNode?) -> ListNode? {
+        var result: ListNode?
+        var node = head
+        while node != nil {
+            let next = node?.next
+            node?.next = result
+            result = node
+            node = next
+        }
+        return result
+    }
+}
